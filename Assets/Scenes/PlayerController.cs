7dbbcd6e27cdb62a50310b32aab687ce5bd795c6
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2023-11-02 08:34:57 -0400

    almost finished walking script

---
+++ Assets/Scenes/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float _playerSpeed = 5f;
+
+    public bool canMove = true;
+    public bool moving = false;
+
+    Animator anim;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (moving)
+        {
+            anim.SetBool("walk", true);
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
+
+        if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow)))
+        {
+            moving = false;
+        }
+
+        if(canMove)
+        {
+
+        }
+    }
+}
