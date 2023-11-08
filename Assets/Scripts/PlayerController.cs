db23cc682f0ad63375b45d8a0c2c4260915dae39
Davidrodvill <144342667+Davidrodvill@users.noreply.github.com>
2023-11-08 08:38:39 -0500

    sum

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,74 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
-    public float _playerSpeed = 5f;
-
+    [SerializeField] float _speed;
     public bool canMove = true;
-    public bool moving = false;
+    Rigidbody2D _rb;
 
-    public Vector3[] _playerPositions = { new Vector3(0, 0, 0), new Vector3(50, 40, 0) };
-
-    Animator anim;
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        _rb = GetComponent<Rigidbody2D>();
 
-    void FixedUpdate()
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
     {
-        if (moving)
+        if (!canMove)
         {
-            anim.SetBool("walk", true);
-        }
-        else
-        {
-            anim.SetBool("walk", false);
-        }
-
-        if (!(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.RightArrow)))
-        {
-            moving = false;
-        }
-
-        if(canMove)
-        {
-            Vector3 pos = Camera.main.WorldToViewportPoint(
-                transform.position);
-            if (Input.GetAxisRaw("Vertical") > 0 &&
-                    pos.y < 0.96f)//up
-            {
-                transform.position += new Vector3(
-                    0, _playerSpeed * Time.deltaTime);
-                moving = true;
-            }
-            if (Input.GetAxisRaw("Vertical") < 0 &&
-                pos.y > 0.11f) //down
-            {
-                transform.position -= new Vector3(
-                    0, _playerSpeed * Time.deltaTime);
-                moving = true;
-            }
-            if (Input.GetAxisRaw("Horizontal") > 0 &&
-                pos.x < 0.96f) //right
-            {
-                transform.position += new Vector3(
-                    _playerSpeed * Time.deltaTime, 0);
-                moving = true;
-            }
-            if (Input.GetAxisRaw("Horizontal") < 0 &&
-               pos.x > 0.1f) //left
-            {
-                transform.position -= new Vector3(
-                    _playerSpeed * Time.deltaTime, 0);
-                moving = true;
-            }
-
+            return;
         }
     }
 }
